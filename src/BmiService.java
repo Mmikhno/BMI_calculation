c3cffd889a2d761592abc1b0c48745f20dea13eb
Maria Mikhno <mmikhno89@gmail.com>
2022-08-09 19:03:21 +0300

    Reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
-    public double  calculate (double height, double weight) {
+    public double calculate(double height, double weight) {
 
         double bmi;
-        bmi = weight/(height*height);
+        bmi = weight / (height * height);
         return bmi;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         BmiService service = new BmiService();
         double height = 1.8;
         double weight = 60;
-        double bmi = service.calculate(height,weight);
+        double bmi = service.calculate(height, weight);
         //вывод с двумя знаками после точки
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
         String result = decimalFormat.format(bmi);
